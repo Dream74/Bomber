ce93be71f04d10a02fb9a4607e7efd3d109d0456
Jing-Yuan <a13114337@gmail.com>
2013-11-22 22:47:29 +0800

    add Square

---
+++ Bomber/Square.h
@@ -0,0 +1,37 @@
+//
+//  Square.h
+//  Bomber
+//
+//  Created by luoboy on 13/11/22.
+//  Copyright (c) 2013年 CYCU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Square : NSObject
+
+enum OBJ_TYPE { NOTHING = 0 ,  ITEM, PLAYER , BOMB , BRICK, LENGTH } ;
+
+@property (nonatomic ) int x ;
+@property (nonatomic ) int y ;
+
+@property (nonatomic ) int pixal_x ;
+@property (nonatomic ) int pixal_y ;
+
+@property (nonatomic ) int pixal_x_max ;
+@property (nonatomic ) int pixal_y_max ;
+
+@property (nonatomic ) int exsitObj ; // use enum to control
+
+@property (nonatomic ) NSMutableArray * objList ;  // the square exsit what
+
+@property (nonatomic ) bool canIn ;
+
++(CGPoint) isExistInThisSquare : (int) X : (int) Y ;
+-(void) initalLacation : (int) X : (int) Y ;
+
+-(void) putThingInSquare : Type : (int) type PutObject : (id) obj ;
+-(void) removeThingFromSquare ;
+
+
+@end
---
+++ Bomber/Square.m
@@ -0,0 +1,69 @@
+//
+//  Square.m
+//  Bomber
+//
+//  Created by luoboy on 13/11/22.
+//  Copyright (c) 2013年 CYCU. All rights reserved.
+//
+
+#import "Square.h"
+
+@implementation Square
+
+@synthesize x ;
+@synthesize y ;
+@synthesize pixal_x ;
+@synthesize pixal_y ;
+@synthesize pixal_x_max ;
+@synthesize pixal_y_max ;
+
+@synthesize exsitObj ;
+@synthesize objList ;
+
+@synthesize canIn ;
+
+-(void) initalLacation: (int) X : (int) Y {
+    x = X ;
+    y = Y ;
+    
+    pixal_x = x * 32 ;
+    pixal_y = y * 32 ;
+    
+    pixal_x_max = pixal_x + 32 ;
+    pixal_y_max = pixal_y + 32 ;
+    
+    exsitObj = NOTHING ;
+    
+    objList = [[NSMutableArray alloc] init ] ;
+    
+    canIn = true ;
+    
+}
+
++(CGPoint) isExistInThisSquare:(int)X :(int)Y {
+    int x = X / 33 ;
+    int y = Y / 33 ;
+    return CGPointMake(x, y) ;
+}
+
+
+-(void) putThingInSquare : Type : (int) type PutObject : (id) obj {
+    exsitObj = type ;
+    [ objList addObject:obj ] ;
+    if ( type <= PLAYER ) canIn = true ;
+    else canIn = false ;
+    
+}
+
+-(void) removeThingFromSquare {
+    exsitObj = NOTHING ;
+    objList = [[NSMutableArray alloc] init ] ;
+    canIn = true ;
+}
+
+
+
+
+
+
+@end
